3def7e7674d7e7793b3defa9807b1c577e0fe57c
aarya-dev003 <aaryagupta2003@gmail.com>
2023-10-08 00:06:03 +0530

    fixed back button in plan trip

--- app/src/main/java/com/example/bharatyatrisathi/PlanTrip.java
+++ app/src/main/java/com/example/bharatyatrisathi/PlanTrip.java
@@ -2,13 +2,31 @@ package com.example.bharatyatrisathi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PlanTrip extends AppCompatActivity {
 
+    ImageButton back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_trip);
+        back = findViewById(R.id.backButton);
+
+
+        back.setOnClickListener(new View.OnClickListener() {
+
+
+            Intent toMain = new Intent(getApplicationContext(), Homepage.class);
+            @Override
+            public void onClick(View v) {
+                startActivity(toMain);
+            }
+
+
+        });
     }
 }
