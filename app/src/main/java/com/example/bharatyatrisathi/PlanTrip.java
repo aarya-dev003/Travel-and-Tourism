198caecc8fa02abf0ae08850d6a03fb8c8d9fd7e
AnshGaur-dev <anshgaur914@gmail.com>
2023-10-09 23:47:34 +0530

    Plan trip edts

--- app/src/main/java/com/example/bharatyatrisathi/PlanTrip.java
+++ app/src/main/java/com/example/bharatyatrisathi/PlanTrip.java
@@ -5,16 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class PlanTrip extends AppCompatActivity {
 
+   EditText place_to_go_edt,start_date_edt,trip_days_edt,guys_on_trip_edt;
     ImageButton back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_trip);
         back = findViewById(R.id.backButton);
+        place_to_go_edt=findViewById(R.id.place_to_go_edt);
+        start_date_edt=findViewById(R.id.start_date_edt);
+        trip_days_edt=findViewById(R.id.trip_days_edt);
+        guys_on_trip_edt=findViewById(R.id.guys_on_trip_edt);
+
+        String plantrip = "I want to go"+place_to_go_edt.
+                getText().
+                toString()+" start from "+start_date_edt.
+                getText().
+                toString()+" and no of days are"+trip_days_edt.
+                getText().
+                toString()+"no of travelers"+guys_on_trip_edt
+                .getText().
+                toString();
+
+
+
 
 
         back.setOnClickListener(new View.OnClickListener() {
@@ -25,8 +44,6 @@ public class PlanTrip extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(toMain);
             }
-
-
         });
     }
 }
