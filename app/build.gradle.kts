1c4fd01ea67c18bfd0986c577fe71a1a7675b6fe
aarya-dev003 <aaryagupta2003@gmail.com>
2023-10-14 22:10:21 +0530

    added sign in with google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,20 +38,26 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment:2.7.4")
+    implementation("androidx.navigation:navigation-fragment:2.5.3")
     implementation("de.hdodenhof:circleimageview:3.1.0")
-    implementation("androidx.navigation:navigation-ui:2.7.4")
+    implementation("androidx.navigation:navigation-ui:2.5.3")
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("com.google.firebase:firebase-inappmessaging-display-ktx:20.3.5")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.firebase:firebase-config-ktx:21.4.1")
+    implementation("com.google.firebase:firebase-messaging-ktx:23.2.1")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    implementation("com.google.firebase:firebase-database-ktx:20.2.2")
 }
--- app/src/main/java/com/example/bharatyatrisathi/Loginpage.java
+++ app/src/main/java/com/example/bharatyatrisathi/Loginpage.java
@@ -1,7 +1,9 @@
 package com.example.bharatyatrisathi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,9 +11,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Loginpage extends AppCompatActivity {
-EditText editTextemail,editTextpassword;
-Button submit,signup;
+    EditText editTextemail,editTextpassword;
+    Button submit,signup,SignIn;
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    private static final int RC_SIGN_IN = 1;
+
+
+    GoogleSignInClient mgoogleSignInClient;
+    ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +45,18 @@ Button submit,signup;
         editTextpassword=findViewById(R.id.editTextTextPassword);
         submit=findViewById(R.id.submitbutton);
         signup=findViewById(R.id.Login_signup);
+        SignIn=findViewById(R.id.SignIn);
+
+        database= FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog( Loginpage.this);
+        progressDialog.setTitle("Creating Account");
+        progressDialog.setMessage("We are creating your account");
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build();
+        mgoogleSignInClient= GoogleSignIn.getClient(this,gso);
+
 
 
         submit.setOnClickListener(new View.OnClickListener() {
@@ -50,8 +86,69 @@ Button submit,signup;
                 startActivity(Login_to_Register);
             }
         });
+        SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
 
 
 
     }
+
+    private void signIn() {
+        Intent signInIntent = mgoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent,RC_SIGN_IN);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RC_SIGN_IN:
+                try {
+                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    FirebaseAuthWithGoogle(account.getIdToken());
+
+                } catch (ApiException e) {
+                    throw new RuntimeException();
+                }
+                break;
+        }
+
+
+    }
+
+    private void FirebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken,null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+
+                    Users users = new Users();
+                    users.setUserId(user.getUid());
+                    users.setName(user.getDisplayName());
+                    users.setProfile(user.getPhotoUrl().toString());
+
+                    database.getReference().child("Users").child(user.getUid()).setValue(users);
+                    Intent intent = new Intent(Loginpage.this,Homepage.class);
+                    startActivity(intent);
+
+
+
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Something went wrong",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
 }
