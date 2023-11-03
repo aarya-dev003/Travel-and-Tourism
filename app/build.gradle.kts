9709d94accb0a1950c4e99f3c6dca10ee949a186
aarya-dev003 <aaryagupta2003@gmail.com>
2023-11-03 13:16:07 +0530

    fixed google sign in

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("com.google.firebase:firebase-inappmessaging-display-ktx:20.3.5")
     implementation("com.google.firebase:firebase-auth-ktx:22.2.0")
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
     implementation("com.google.firebase:firebase-config-ktx:21.4.1")
     implementation("com.google.firebase:firebase-messaging-ktx:23.2.1")
     implementation("com.google.firebase:firebase-firestore-ktx:24.8.1")
@@ -55,6 +55,8 @@ dependencies {
    
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
+
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.firebase:firebase-auth:22.2.0")
 
     testImplementation("junit:junit:4.13.2")
